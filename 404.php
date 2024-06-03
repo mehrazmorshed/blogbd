2573d74a21dc6df2e0ff6e2c67261ff3be54c09d
Mehraz Morshed <mehrazmorshed@gmail.com>
2024-06-03 13:24:48 +0600

    theme update

--- 404.php
+++ 404.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Bloggy
+ * @since 1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+
 get_header();
 ?>
     <div id="primary" class="content-area">
